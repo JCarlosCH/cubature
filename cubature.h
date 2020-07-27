4596943db39c7b84f52fda2d558f4c441ef2b12c
JCarlosCH <jncarlosch@gmail.com>
2020-07-26 20:33:37 -0500

    Acepta un número mínimo de evaluaciones y tres funciones para evaluar la función temporal

--- cubature.h
+++ cubature.h
@@ -36,85 +36,85 @@ extern "C"
 {
 #endif /* __cplusplus */
 
-/* USAGE: Call hcubature or pcubature with your function as described
-          in the README file. */
+  /* USAGE: Call hcubature or pcubature with your function as described
+     in the README file. */
 
-/* a vector integrand - evaluates the function at the given point x
-   (an array of length ndim) and returns the result in fval (an array
-   of length fdim).   The void* parameter is there in case you have
-   to pass any additional data through to your function (it corresponds
-   to the fdata parameter you pass to cubature).  Return 0 on
-   success or nonzero to terminate the integration. */
-typedef int (*integrand) (unsigned ndim, const double *x, void *,
-                          unsigned fdim, double *fval);
-
-/* a vector integrand of a vector of npt points: x[i*ndim + j] is the
-   j-th coordinate of the i-th point, and the k-th function evaluation
-   for the i-th point is returned in fval[i*fdim + k].  Return 0 on success
-   or nonzero to terminate the integration. */
-typedef int (*integrand_v) (unsigned ndim, size_t npt,
-			    const double *x, void *,
+  /* a vector integrand - evaluates the function at the given point x
+     (an array of length ndim) and returns the result in fval (an array
+     of length fdim).   The void* parameter is there in case you have
+     to pass any additional data through to your function (it corresponds
+     to the fdata parameter you pass to cubature).  Return 0 on
+     success or nonzero to terminate the integration. */
+  typedef int (*integrand) (unsigned ndim, const double *x, void *,
 			    unsigned fdim, double *fval);
 
-/* Different ways of measuring the absolute and relative error when
-   we have multiple integrands, given a vector e of error estimates
-   in the individual components of a vector v of integrands.  These
-   are all equivalent when there is only a single integrand. */
-typedef enum {
-     ERROR_INDIVIDUAL = 0, /* individual relerr criteria in each component */
-     ERROR_PAIRED, /* paired L2 norms of errors in each component,
-		      mainly for integrating vectors of complex numbers */
-     ERROR_L2, /* abserr is L_2 norm |e|, and relerr is |e|/|v| */
-     ERROR_L1, /* abserr is L_1 norm |e|, and relerr is |e|/|v| */
-     ERROR_LINF /* abserr is L_\infty norm |e|, and relerr is |e|/|v| */
-} error_norm;
+  /* a vector integrand of a vector of npt points: x[i*ndim + j] is the
+     j-th coordinate of the i-th point, and the k-th function evaluation
+     for the i-th point is returned in fval[i*fdim + k].  Return 0 on success
+     or nonzero to terminate the integration. */
+  typedef int (*integrand_v) (unsigned ndim, size_t npt,
+			      const double *x, void *,
+			      unsigned fdim, double *fval);
 
-/* Integrate the function f from xmin[dim] to xmax[dim], with at most
-   maxEval function evaluations (0 for no limit), until the given
-   absolute or relative error is achieved.  val returns the integral,
-   and err returns the estimate for the absolute error in val; both
-   of these are arrays of length fdim, the dimension of the vector
-   integrand f(x). The return value of the function is 0 on success
-   and non-zero if there  was an error. */
+  /* Different ways of measuring the absolute and relative error when
+     we have multiple integrands, given a vector e of error estimates
+     in the individual components of a vector v of integrands.  These
+     are all equivalent when there is only a single integrand. */
+  typedef enum {
+		ERROR_INDIVIDUAL = 0, /* individual relerr criteria in each component */
+		ERROR_PAIRED, /* paired L2 norms of errors in each component,
+				 mainly for integrating vectors of complex numbers */
+		ERROR_L2, /* abserr is L_2 norm |e|, and relerr is |e|/|v| */
+		ERROR_L1, /* abserr is L_1 norm |e|, and relerr is |e|/|v| */
+		ERROR_LINF /* abserr is L_\infty norm |e|, and relerr is |e|/|v| */
+  } error_norm;
 
-/* adapative integration by partitioning the integration domain ("h-adaptive")
-   and using the same fixed-degree quadrature in each subdomain, recursively,
-   until convergence is achieved. */
-int hcubature(unsigned fdim, integrand f, void *fdata,
-	      unsigned dim, const double *xmin, const double *xmax, 
-	      size_t maxEval, double reqAbsError, double reqRelError, 
-	      error_norm norm,
-	      double *val, double *err);
+  /* Integrate the function f from xmin[dim] to xmax[dim], with at most
+     maxEval function evaluations (0 for no limit), until the given
+     absolute or relative error is achieved.  val returns the integral,
+     and err returns the estimate for the absolute error in val; both
+     of these are arrays of length fdim, the dimension of the vector
+     integrand f(x). The return value of the function is 0 on success
+     and non-zero if there  was an error. */
 
-/* as hcubature, but vectorized integrand */
+  /* adapative integration by partitioning the integration domain ("h-adaptive")
+     and using the same fixed-degree quadrature in each subdomain, recursively,
+     until convergence is achieved. */
+  int hcubature(unsigned fdim, integrand f, void *fdata,
+		unsigned dim, const double *xmin, const double *xmax, size_t minEval,
+		size_t maxEval, double reqAbsError, double reqRelError,
+		error_norm norm,
+		double *val, double *err);
+
+  /* as hcubature, but vectorized integrand */
   int hcubature_v(unsigned fdim, integrand_v f, integrand_v func_first, integrand_v funct, void *fdata,
-		unsigned dim, const double *xmin, const double *xmax, 
-		size_t maxEval, double reqAbsError, double reqRelError, 
-		error_norm norm,
-		double *val, double *err);
+		  unsigned dim, const double *xmin, const double *xmax, size_t minEval,
+		  size_t maxEval, double reqAbsError, double reqRelError,
+		  error_norm norm,
+		  double *val, double *err);
 
-/* adaptive integration by increasing the degree of (tensor-product
-   Clenshaw-Curtis) quadrature rules ("p-adaptive"), rather than
-   subdividing the domain ("h-adaptive").  Possibly better for
-   smooth integrands in low dimensions. */
-int pcubature_v_buf(unsigned fdim, integrand_v f, void *fdata,
-		    unsigned dim, const double *xmin, const double *xmax,
-		    size_t maxEval, 
-		    double reqAbsError, double reqRelError,
-		    error_norm norm,
-		    unsigned *m,
-		    double **buf, size_t *nbuf, size_t max_nbuf,
-		    double *val, double *err);
-int pcubature_v(unsigned fdim, integrand_v f, void *fdata,
-		unsigned dim, const double *xmin, const double *xmax, 
-		size_t maxEval, double reqAbsError, double reqRelError, 
+  /* adaptive integration by increasing the degree of (tensor-product
+     Clenshaw-Curtis) quadrature rules ("p-adaptive"), rather than
+     subdividing the domain ("h-adaptive").  Possibly better for
+     smooth integrands in low dimensions. */
+  int pcubature_v_buf(unsigned fdim, integrand_v f, void *fdata,
+		      unsigned dim, const double *xmin, const double *xmax,
+		      size_t maxEval,
+		      double reqAbsError, double reqRelError,
+		      error_norm norm,
+		      unsigned *m,
+		      double **buf, size_t *nbuf, size_t max_nbuf,
+		      double *val, double *err);
+  int pcubature_v(unsigned fdim, integrand_v f, void *fdata,
+		  unsigned dim, const double *xmin, const double *xmax,
+		  size_t maxEval, double reqAbsError, double reqRelError,
+		  error_norm norm,
+		  double *val, double *err);
+  int pcubature(unsigned fdim, integrand f, void *fdata,
+		unsigned dim, const double *xmin, const double *xmax,
+		size_t maxEval, double reqAbsError, double reqRelError,
 		error_norm norm,
 		double *val, double *err);
-int pcubature(unsigned fdim, integrand f, void *fdata,
-	      unsigned dim, const double *xmin, const double *xmax, 
-	      size_t maxEval, double reqAbsError, double reqRelError, 
-	      error_norm norm,
-	      double *val, double *err);
 
 #ifdef __cplusplus
 }  /* extern "C" */
